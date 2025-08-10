b3925cc30fd437ce780112c7048e5646352977fb
Juan David <juandaguti@icloud.com>
2025-08-10 16:59:27 +0100

    Initial Commit

---
+++ SwiftfulCrypto/SwiftfulCryptoApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftfulCryptoApp.swift
+//  SwiftfulCrypto
+//
+//  Created by Juan David Gutierrez Olarte on 10/08/25.
+//
+
+import SwiftUI
+
+@main
+struct SwiftfulCryptoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
